0377b67746ed4a43a1da9c24f312ea166b1b591f
motun <motunmarteen@gmail.com>
2023-04-26 16:42:20 +0100

    Social Fashion Marketplace

---
+++ Readme.d
@@ -0,0 +1,1 @@
+Social Fashion Marketplace 
---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".auto-type", {
+    strings: ["Social\nFashion\nMarketplace", "Sell/Swap/Rent\nPre-owned\nItems", "Community\nDriven\nSpace", "Dynamic &\nCompetitve\nPricing", "Virtual\nFitting\nAssistant", "Virtual\nBody\nMeasurement", "Imagine\nCreative\nStudio", "Services\nReal-Time\nMonitoring", "Personalized\nAI\nFashion\nRecommender", "Virtual\nWardrobe\nAssistant", "Customer\nExperience\nVerification", "Last-Mile\nOrder\nFulfilment"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+});
+
